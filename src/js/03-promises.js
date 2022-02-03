8771bd621a7cdb7bdd206558d9c1a2da754acd02
Oleksii Verbytskyi <alverba88@gmail.com>
2022-02-03 11:58:37 +0200

    finished third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,7 +4,7 @@ const dataPromiseForm =document.querySelector('.form');
 
 const createPromise=(position, delay)=>{
   const shouldResolve = Math.random() > 0.3;
-// position - номер Промиса, Дэлэй - задержка=delay+step
+
   return new Promise((resolve, reject)=>{
     setTimeout(()=>{
       if(shouldResolve){        
@@ -20,15 +20,12 @@ const createPromise=(position, delay)=>{
 const createPromiseHandler=(e)=>{
   e.preventDefault();
   const { elements } = e.currentTarget;
+
   let delay=+elements.delay.value,
       step=+elements.step.value,
       amount=+elements.amount.value;
 
-        // { delay, step, amount } = elements;
-  // position - номер Промиса, Дэлэй - задержка=delay+step 
-
   for(let i=0;i<+amount;i+=1){
-
     
     createPromise(i,delay)
     .then(({ position, delay })=>{
@@ -39,7 +36,6 @@ const createPromiseHandler=(e)=>{
     });
     delay+=step;
   }
-
 }
 
 dataPromiseForm.addEventListener('submit', createPromiseHandler);
