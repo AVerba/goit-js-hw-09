9f7457a33d930d4b75f0a17cf3e8bbbf949b4406
Oleksii Verbytskyi <alverba88@gmail.com>
2022-02-03 08:52:13 +0200

    start third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
+
+import Notiflix from 'notiflix';
+const dataPromiseForm =document.querySelector('.form');
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return promise= new Promise((res, reject)=>{
+    setTimeout(()=>{
+      if (shouldResolve) {
+        // Fulfill
+      } else {
+        // Reject
+      }
+    },delay)
+  })
+ 
 }
+
+const createpromiseHandler=(e)=>{
+  e.preventDefault();
+
+  const { elements } = e.currentTarget,
+        { delay, step, amount } = elements;
+        
+  console.log(+delay.value, +step.value, +amount.value);
+}
+
+
+dataPromiseForm.addEventListener('submit', createpromiseHandler)
